e1f4b10d3103ba6da3f64fca976dde431f3d00f8
Stephen Kirkland <sk01417@uga.edu>
2017-02-02 14:03:22 -0500

    not much has changed...just want to back things up

--- University/University.cs
+++ University/University.cs
@@ -9,7 +9,8 @@ namespace University
 {
     class University
     {
-        public delegate List<Student> GetStudents();
+        public delegate List<Student> GetStudents(); //we want the admin to be the ONLY one to close the 
+                                                     // course
         
 
     }
--- University/users/Administrator.cs
+++ University/users/Administrator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using University.Courses;
 
 namespace University.Users
 {
@@ -13,6 +14,10 @@ namespace University.Users
             return base.ToString();
         }
 
+        /// <summary>
+        /// only the admin is able to set a new password
+        /// </summary>
+        /// <param name="password">the new password string to be set for a particular student</param>
         public void setPassword(string password)
         {
             this.password = password;
