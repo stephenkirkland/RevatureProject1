39171da4616f53492ab17d6c1c52706aa0fb48c7
Stephen Kirkland <sk01417@uga.edu>
2017-02-03 13:19:58 -0500

    created a new project (RegApp.UnitTests.csproj) and a new class (UnitTests.cs)

---
+++ RegApp.UnitTests/UnitTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace RegApp.UnitTests
+{
+
+    [TestFixture]
+    public class UnitTests
+    {
+        [Test]
+        public void AddNumbers()
+        {
+            // aragnge
+
+            // act
+
+            // assert
+        }
+    }
+}
