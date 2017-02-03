3cbb459c8eb79f241390afe488792fbd8018ec63
Stephen Kirkland <sk01417@uga.edu>
2017-02-02 19:01:25 -0500

    nothing...

--- University/University.cs
+++ University/University.cs
@@ -9,9 +9,6 @@ namespace University
 {
     class University
     {
-        public delegate List<Student> GetStudents(); //we want the admin to be the ONLY one to close the 
-                                                     // course
         
-
     }
 }
